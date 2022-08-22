183f3d2125380acb468b10844437d223d180198a
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-22 11:08:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ y = df.Species
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Model building
-rf = RandomForestClassifier(max_depth=2, max_features=st_max_features, n_estimators=st_n_estimators, random_state=42)
+rf = RandomForestClassifier(max_depth=2, max_features=4, n_estimators=200, random_state=42)
 rf.fit(X_train, y_train)
 
 # Apply model to make predictions
