f2b838d936f086cb9fccb2281cb4ae4b57e7ba97
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-22 11:30:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,10 @@ df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/ir
 
 # Input widgets
 st.sidebar.subheader('Input features')
-#sepal_length = st.sidebar.slider('Sepal length', 0.0, 1.0, 0.1)
-#sepal_length = st.sidebar.slider('Sepal length', df['Sepal.Length'].min(), df['Sepal.Length'].max(), df['Sepal.Length'].median())
-#sepal_width = st.sidebar.slider('Sepal width', df['Sepal.Width'].min(), df['Sepal.Width'].max(), df['Sepal.Width'].median())
-#petal_length = st.sidebar.slider('Petal length', df['Petal.Length'].min(), df['Petal.Length'].max(), df['Petal.Length'].median())
-#petal_width = st.sidebar.slider('Petal width', df['Petal.Width'].min(), df['Petal.Width'].max(), df['Petal.Width'].median())
+sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.8)
+sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.1)
+petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 3.8)
+petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 1.2)
 
 # Separate to X and y
 X = df.drop('Species', axis=1)
