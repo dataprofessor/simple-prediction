9efe1caff566ceecc8ef0044eacf342aa65f2032
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-22 11:44:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,6 @@ rf.fit(X_train, y_train)
 y_pred = rf.predict([[sepal_length, sepal_width, petal_length, petal_width]])
 
 # Print prediction results
+input_feature = pd.DataFrame([[sepal_length, sepal_width, petal_length, petal_width]])
+st.write()
 st.write(y_pred)
