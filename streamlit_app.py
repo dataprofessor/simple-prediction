076ebfb163e5d096391eb66bff9d41034157e85e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-22 11:03:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ rf = RandomForestClassifier(max_depth=2, max_features=st_max_features, n_estimat
 rf.fit(X_train, y_train)
 
 # Apply model to make predictions
-y_pred = rf.predict([sepal_length, sepal_width, petal_length, petal_width])
+#y_pred = rf.predict([sepal_length, sepal_width, petal_length, petal_width])
 
 # Print prediction results
 st.write(st_sepal_length)
